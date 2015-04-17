60a8bb98daf1c0679423d52f2366d927d91c03b8
Marek Radonsky <radonsky@gmail.com>
2015-04-16 20:18:45 -0700

    Added /voice action generating TwiML to call a number given by env. variable

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,12 +3,15 @@ package controllers
 import play.api._
 import play.api.mvc._
 import com.twilio.sdk.client.TwilioCapability
+import com.twilio.sdk.verbs._
 
 object Application extends Controller {
 
   val AccountSid = System.getenv("TWILIO_ACCOUNT_SID")
   val AuthToken = System.getenv("TWILIO_AUTH_TOKEN")
   val ApplicationSid = System.getenv("TWILIO_APPLICATION_SID")
+  val PhoneNumber = System.getenv("TWILIO_PHONE_NUMBER")
+  val CallerId = System.getenv("TWILIO_CALLER_ID")
   
   def index = Action {
     val capability = new TwilioCapability(AccountSid, AuthToken)
@@ -18,4 +21,13 @@ object Application extends Controller {
     Ok(views.html.index(token))
   }
 
+  def voice = Action {
+    val twiml = new TwiMLResponse()
+    val dial = new Dial()
+    dial.append(new Number(PhoneNumber))
+    dial.setCallerId(CallerId)
+    twiml.append(dial)
+    Ok(twiml.toXML()).as("application/xml")
+  }
+  
 }
